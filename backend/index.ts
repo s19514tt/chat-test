f91329eb1dda90346d62539b4613e9284738e719
s19514tt <s19514tt@sfc.keio.ac.jp>
2022-10-09 17:59:42 +0900

    first commit

---
+++ backend/index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+
+const app = express()
+
+const server = require('http').createServer(app)
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:8080',
+    methods: ['*'],
+  },
+})
+
+io.on('connection', (socket) => {
+  socket.on('message', (msg) => {
+    console.log(msg)
+    io.emit('msg', msg)
+  })
+})
+server.listen(3000)
---
+++ frontend/src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import io from "socket.io-client";
+
+const name = ref<string>('')
+const contents = ref<string>('')
+const msgs = ref<Array<{
+  content: string,
+  name: string
+}>>([])
+/* 
+{
+  content: string,
+  name: string
+}
+*/
+
+const socket = io("http://localhost:3000/");
+
+socket.on('msg', (msg) => {
+  msgs.value.push(JSON.parse(msg))
+  console.log(msgs.value);
+})
+
+function add() {
+  //ABCの中身をhello.valueに置き換える
+  const json = JSON.stringify({
+    content: contents.value,
+    name: name.value
+  })
+  socket.send(json)
+}
+
+
+</script>
+<template>
+  <div v-for="msg in msgs">{{msg.name}}: {{msg.content}}</div>
+  <input v-model="name" />
+  <input v-model="contents" /><button @click="add()">送信</button>
+</template>
